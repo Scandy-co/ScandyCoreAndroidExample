450bcd113a6f3f94d4be12425a36b1ee9bdb93bc
H. Cole Wiley <cole@scandy.co>
2017-03-31 12:11:00 -0500

    fixed no copyright

--- app/src/androidTest/java/co/scandy/example/scandycoreandroidexample/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/co/scandy/example/scandycoreandroidexample/ExampleInstrumentedTest.java
@@ -1,3 +1,13 @@
+/****************************************************************************\
+ * Copyright (C) 2017 Scandy
+ *
+ * THIS CODE AND INFORMATION ARE PROVIDED "AS IS" WITHOUT WARRANTY OF ANY
+ * KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND/OR FITNESS FOR A
+ * PARTICULAR PURPOSE.
+ *
+ \****************************************************************************/
+
 package co.scandy.example.scandycoreandroidexample;
 
 import android.content.Context;
